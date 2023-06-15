0e16fba0adfa45619084ba61d7b0088353735858
fullstacksree <114227478+fullstacksree@users.noreply.github.com>
2023-06-15 21:34:10 +0530

    Final Submission

--- Lesson9/scripts/prophets.js
+++ Lesson9/scripts/prophets.js
@@ -16,24 +16,39 @@ async function getProphetData() {
       // Create elements to add to the div.cards element
       let card = document.createElement('section');
       let h2 = document.createElement('h2');
-      let h3 = document.createElement('h3');
+      let birthDate = document.createElement('p');
+      let birthPlace = document.createElement('p');
+      let hrline = document.createElement('hr');
       let portrait = document.createElement('img');
-  
+      let order = ``
+
+      if (prophet.order == 1) {
+        order = `${prophet.order}st`
+      } else if (prophet.order == 2) {
+        order = `${prophet.order}nd`
+      } else if (prophet.order == 3) {
+        order = `${prophet.order}rd`
+      } else {
+        order = `{prophet.order}th`
+      }
+
       // Build the h2 content out to show the prophet's full name - finish the template string
       h2.textContent = `${prophet.name} ${prophet.lastname}`;
-      h3.textContent = `${prophet.birthdate}, ${prophet.birthplace}`;
-        
-      // Build the image portrait by setting all the relevant attribute
+      birthDate.textContent = `Birth Date: ${prophet.birthdate}`
+      birthPlace.textContent = `Birth Place: ${prophet.birthplace}`
       
+      // Build the image portrait by setting all the relevant attribute
       portrait.setAttribute('src', prophet.imageurl);
-      portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+      portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname} - ${order} Latter-day President`);
       portrait.setAttribute('loading', 'lazy');
       portrait.setAttribute('width', '340');
       portrait.setAttribute('height', '440');
   
       // Append the section(card) with the created elements
       card.appendChild(h2);
-      card.appendChild(h3);
+      card.appendChild(birthDate);
+      card.appendChild(birthPlace);
+      card.appendChild(hrline);
       card.appendChild(portrait);
   
       cards.appendChild(card);
